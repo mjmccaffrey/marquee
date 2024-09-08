8d8a74357a725a504cb5a1ff268253d3b77c6b6a
Mike McCaffrey <mjmccaffrey@gmail.com>
2024-09-08 14:27:18 -0400

    wip

--- relayboards.py
+++ relayboards.py
@@ -39,7 +39,7 @@ class RelayBoard:
            rightmost bit of the binary / hex value."""
         print(device_pattern)
         relay_pattern = ''.join(
-            device_pattern[self._bit_to_device[b]]
+            str(device_pattern[self._bit_to_device[b]])
             if b in self._bit_to_device else '0'
             for b in range(RELAY_COUNT)
         )
