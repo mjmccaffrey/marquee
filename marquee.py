68ea23c4ba7b9424c41ec34735e58c46d568029b
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-02-03 19:30:48 -0500

    WIP

--- marquee.py
+++ marquee.py
@@ -102,7 +102,8 @@ def register_modes(player: players.Player):
         player.do_sequence(seq_all_on)
         player.do_sequence(seq_all_off, relay_override=RelayOverride(concurrent=True))
         for i, dimmer in enumerate(player._sign._dimmers):
-            dimmer.set(level=(i+1)*10, transition=10, wait=True)
+            print(i)
+            dimmer.set(level=(i+1)*10, transition=10)
 
 def is_valid_light_pattern(arg):
     """ Return True if arg is a valid light pattern, 
