275c41dd2a5d0223807cf4eb248f4a67de0d3216
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-02-11 11:56:10 -0500

    type hints

---
+++ argparse_patch.py
@@ -0,0 +1,15 @@
+import argparse
+
+class ArgumentParserNeverExit(argparse.ArgumentParser):
+    """
+    !!!!
+    https://bugs.python.org/issue41255
+    https://github.com/python/cpython/issues/103498
+    https://stackoverflow.com/questions/67890157/python-3-9-1-argparse-exit-on-error-not-working-in-certain-situations
+    https://stackoverflow.com/questions/69108632/unable-to-catch-exception-error-for-argparse
+    """
+    def error(self, message):
+        raise ValueError(f"Argparse error:{message}")
+
+    def exit(self, status=0, message=None):
+        raise ValueError(f"Argparse error:{status}:{message}")
