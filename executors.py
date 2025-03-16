515f7a339d51fc9d2bb504292796d668f5a3f41d
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-03-16 16:23:52 -0400

    connect remote

--- executors.py
+++ executors.py
@@ -33,10 +33,10 @@ def create_sign() -> Sign:
     relaymodule: NumatoRL160001 = NumatoRL160001("/dev/ttyACM0", ALL_RELAYS)
     buttons = [
         Button('body_mode_select', _Button(pin=17, bounce_time=0.10), SIGUSR1),
-        Button('remote_mode_select', _Button(pin=18, bounce_time=0.10)),
-        Button('remote_mode_up', _Button(pin=23, bounce_time=0.10)),
-        Button('remote_mode_down', _Button(pin=24, bounce_time=0.10)),
-        Button('remote_demo_mode', _Button(pin=25, bounce_time=0.10)),
+        Button('remote_mode_select', _Button(pin=18, pull_up=False, bounce_time=0.10)),
+        Button('remote_mode_up', _Button(pin=23, pull_up=False, bounce_time=0.10)),
+        Button('remote_mode_down', _Button(pin=24, pull_up=False, bounce_time=0.10)),
+        Button('remote_demo_mode', _Button(pin=25, pull_up=False, bounce_time=0.10)),
     ]
     return Sign(
         dimmers=dimmers,
