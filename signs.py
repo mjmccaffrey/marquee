9a5de513e147995a677530e14ae133a29f7e141c
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-04-01 11:22:42 -0400

    WIP

--- signs.py
+++ signs.py
@@ -190,6 +190,7 @@ class Sign:
             self,
             updates: list[tuple[DimmerChannel, int, float]],
     ):
+        print(updates)
         commands = [
             c.make_set_command(
                 brightness=b,
