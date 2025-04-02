a93fd6ae86d6696ce4a8812d8828b3a778f94055
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-04-02 13:45:30 -0400

    WIP

--- finale.py
+++ finale.py
@@ -9,6 +9,7 @@ class Finale(PlayMusicMode):
 
     def execute(self):
         self.intro()
+        self.body()
 
     def intro(self):
         s = self
@@ -16,36 +17,48 @@ class Finale(PlayMusicMode):
             s.Measure(
                 s.Note('♩', s.light(ALL_OFF)),
                 s.Note('♩', s.dimmer(ALL_HIGH)),
-                s.Rest('𝅗𝅥')),
+                s.Rest('𝅗𝅥'),
+            ),
             s.Measure(
-                s.Note('♩', s.light("0100000000"))),
+                s.Note('♩', s.light("0100000000")),
+            ),
             s.Measure(
-                s.Note('♩', s.light("0000000000"))),
+                s.Note('♩', s.light("0000000000")),
+            ),
             s.Measure(
                 s.Note('♩', s.light("1110001000")),
                 s.Rest('♩𝅘𝅥𝅯'),
-                s.Note('♩', s.light("0000000000")))
+                s.Note('♩', s.light("0000000000")),
                 s.Note('♩', s.dimmer(ALL_LOW)),
+            ),
             s.Measure(
-                s.Rest('♩')
-                s.Note('♩', s.light(ALL_ON))),
+                s.Rest('♩'),
+                s.Note('♩', s.light(ALL_ON)),
+            ),
             s.Measure(
                 s.Sequence('♩', LIGHT_COUNT, seq_random_once_each, 
                     ActionParams(action=s.dimmer_seq(100, 2))),
                 beats=8),
             s.Measure(beats=8),
-
+        )
+    
+    def body(self):
+        s = self
+        s.play(
             s.Measure(
                 s.Rest('𝅗𝅥♩♪𝅘𝅥𝅰'),
                 s.Note('𝅘𝅥𝅰', s.relay(0, 1)),
                 s.Note('𝅘𝅥𝅰', s.relay(2, 3)),
-                s.Note('𝅘𝅥𝅰', s.relay(4, 5)),),
+                s.Note('𝅘𝅥𝅰', s.relay(4, 5)),
+            ),
             s.Measure(
                 s.Sequence('♪', 8, seq_rotate, 
-                    pattern="0100001000", clockwise=True),),
+                    pattern="0100001000", clockwise=True),
+            ),
             s.Measure(
                 s.Sequence('♪', 8, seq_rotate, 
-                    pattern="0100001000", clockwise=False),),
+                    pattern="0100001000", clockwise=False),
+            ),
             s.Measure(
                 s.Note('♪', s.light("1110000000"), s.relay(0, 1)),
                 s.Note('♪', s.light("0001000001"),),
