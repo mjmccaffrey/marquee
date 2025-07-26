db7d7bf321c657d5f9b2fe3651f21c95a2146741
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-07-26 09:17:03 -0400

    wip

--- signs_song.py
+++ signs_song.py
@@ -47,8 +47,6 @@ class SignsSong(PlayMusicMode):
         return func
     
     def intro(self):
-        """Signs song intro."""
-        # 𝅝 𝅗𝅥 ♩ ♪ 𝅘𝅥𝅯 𝅘𝅥𝅰 𝄻 𝄼 𝄽 𝄾 𝄿 𝅀
         return section(
             drum_part(
                 '  h𝅝> | '
@@ -64,41 +62,17 @@ class SignsSong(PlayMusicMode):
                  
             ),  
             bell_part(
-                #                     And the
-                '  𝄻 | 𝄻 |  𝄼 𝄽 𝄾 a𝅘𝅥𝅯 b𝅘𝅥𝅯 |'
-                
-                # sign says long-haired freaky people 
+                '  𝄻 | 𝄻 |  𝄼 𝄽 𝄾 a𝅘𝅥𝅯 b𝅘𝅥𝅯 |  '
                 '  c♩ c♩     b♪ b♪      b♪ b𝅘𝅥𝅯 G𝅘𝅥𝅯  |  '
-                
-                # need not apply  so I
-                ' 𝄾 a♪ a♪ 𝄿 G𝅘𝅥𝅯 𝄽 𝄾 a♪  |'
-
-                # tucked my hair up under my hat and I
-                '𝄿 a♪    𝄿 a♪   a♪  b♪ a♪ a♪ d𝅘𝅥𝅯 b𝅘𝅥𝅯 |'
-
-                # went in to ask him why
-                ' d♪      b♪ d♪ d♪   b𝅘𝅥𝅯 a𝅘𝅥𝅯 𝄾 𝄽 |'
-
-                #   He said, you look like a fine, upstanding young 
-                '  𝄿 e𝅘𝅥𝅯 e𝅘𝅥𝅯   e𝅘𝅥𝅯  e𝅘𝅥𝅯   e𝅘𝅥𝅯   e♪  d♪   c♪ c♪     d♪ |'
-
-                # man,  I think you'll do, uh, so I
-                ' d𝅘𝅥𝅯 c♪ a𝅘𝅥𝅯 d♪ d♪       c♩  𝄿 a𝅘𝅥𝅯 c𝅘𝅥𝅯 a𝅘𝅥𝅯  |'
-
-                # took off my hat and said imagine that, huh
-                ' e𝅘𝅥𝅯   e♪  c𝅘𝅥𝅯 e♪      c♪   d♪   d♪    𝄿 e𝅘𝅥𝅯  𝄾 |'
-
-                # me,   working for you,    Oh...
-                '  c♪ 𝄾  c♪      c♪ d♪  𝄾  𝄾  G♪  '  # e♪ 𝄿 e𝅘𝅥𝅯 d𝅘𝅥𝅯 e𝅘𝅥𝅯 d♪ | '
-            ),
+                '  𝄾 a♪ a♪ 𝄿 G𝅘𝅥𝅯 𝄽 𝄾 a♪  |  '
+                '  𝄿 a♪  𝄿 a♪   a♪  b♪ a♪ a♪ d𝅘𝅥𝅯 b𝅘𝅥𝅯  |  '                 ),
             sequence_part(
                 '  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  '
                 '  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  '
                 '  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩  ',
                 sequence(
                     random_each,
-                    measures=10,
-                    special=ActionParams(action=dimmer_sequence_flip(1)),
+                    special=ActionParams(dimmer_sequence_flip(1)),
                 ),
                 sequence(
                     random_each,
