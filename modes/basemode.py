4196ecf441d2dfe469d61864b6e900944a168c9d
Mike McCaffrey <mjmccaffrey@gmail.com>
2025-10-29 16:20:26 -0400

    Trying on hardware

---
+++ modes/basemode.py
@@ -0,0 +1,25 @@
+"""Marquee Lighted Sign Project - basemode"""
+
+from abc import ABC
+from dataclasses import dataclass
+from typing import Callable
+
+from event import Event
+from playerinterface import PlayerInterface
+from .modeinterface import ModeInterface
+
+@dataclass
+class BaseMode(ModeInterface, ABC):
+    """Base for both foreground and background modes."""
+    player: PlayerInterface
+
+    def schedule(self, action: Callable, due: float):
+        """Schedule a new event."""
+        self.player.event_queue.push(
+            Event(
+                action=action,
+                due=due,
+                owner=self,
+            )
+        )
+
