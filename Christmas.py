2b593239ef4d4fe2a3931cfeafeef0c4788eca19
KevinMcCaffrey <kjm2123@gmail.com>
2025-11-30 15:20:16 -0500

    WIP

---
+++ Christmas.py
@@ -0,0 +1,167 @@
+"""Marquee Lighted Sign Project - signs_song"""
+
+import time
+
+from lightset_misc import ALL_OFF, ALL_LOW, ALL_ON
+from modes.playmusicmode import PlayMusicMode
+from music import (
+    act_part, bell_part, drum_part, sequence_part,
+    dimmer_sequence_flip, section, Section, sequence,
+)
+from sequences import all_on, blink_all, random_each
+from specialparams import ActionParams, ChannelParams
+
+
+class SignsSong(PlayMusicMode):
+    """Signs song."""
+
+    def execute(self) -> None:
+        """Perform Signs song."""
+        self.lights.set_channels(brightness=0, force=True)
+        # time.sleep(0.75) !!!
+        self.lights.set_relays(ALL_ON)
+        sections = [
+            self.intro(),
+            self.refrain(1),
+            self.trans(),
+            self.refrain(2),
+        ]
+        for section in sections:
+            section.play(tempo=75)
+        self.player.wait(None)
+
+    def intro(self) -> Section:
+        """Signs song intro."""
+        # 𝅝 𝅗𝅥 ♩ ♪ 𝅘𝅥𝅯 𝅘𝅥𝅰 𝄻 𝄼 𝄽 𝄾 𝄿 𝅀
+        return section(
+            drum_part(
+                '  h𝅝> | '
+                 '  lh♩ lh♩ lh♩ lh♩ | l♪ h♪ l♪ h♪ l♪ h♪ l♪ h♪|  ' # And the
+                 '  lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ |  ' # Sign says long-haired freaky people
+                 '  lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ |  ' # Need not apply so I
+                 '  lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ |  ' # Tucked my hair up under my hat and I
+                 '  lh𝅘𝅥𝅯> l𝅘𝅥𝅯 𝄿 𝄿 lh𝅘𝅥𝅯> l𝅘𝅥𝅯 𝄿 𝄿 hl♪ hl♪ lh♪ lh♪> |  ' # Went in to ask him why
+                 '  lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ |  ' # He said...
+                 '  lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ |  ' # Man, I think
+                 '  lh𝅘𝅥𝅯> l𝅘𝅥𝅯 𝄿 𝄿 lh𝅘𝅥𝅯> l𝅘𝅥𝅯 𝄿 𝄿 hl♪ hl♪ lh♪ lh♪> |  ' # Took off my hat...
+                 '  lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ lh♪> lh♪ | ', # Me, working for you
+                 
+            ),  
+            bell_part(
+                #  Jingle Bells, Jingle Bells,
+                '  b♩ b♩   b♩ 𝄽 | b♩ b♩   b♩ 𝄽 | '
+                
+                #  Jingle all the   way 
+                '  b♩ d♩   G♩ 𝄾 a♪ | b♩ 𝄽 𝄽 𝄽 |  '
+                
+                # Oh what fun it    is to ride in a 
+                ' c♩ c♩    c♩ 𝄿 c♪ | c♩ b♩ b♩    b♪ b♪ |'
+
+                # one horse open  sleigh
+                ' b♩  a♩    a♩ b♩ | a♩ 𝄽 d♩ 𝄽 |'
+
+                #  Jingle Bells, Jingle Bells,
+                '  b♩ b♩   b♩ 𝄽 | b♩ b♩   b♩ 𝄽 | '
+                
+                #  Jingle all the   way 
+                '  b♩ d♩   G♩ 𝄾 a♪ | b♩ 𝄽 𝄽 𝄽 |  '
+                
+                # Oh what fun it    is to ride in a 
+                ' c♩ c♩    c♩ 𝄿 c♪ | c♩ b♩ b♩    b♪ b♪ |'
+
+                # one horse open  sleigh
+                ' d♩  d♩    b♩ G♩ | G♩ 𝄽 𝄽 𝄽 |'
+
+
+
+
+
+
+
+ 
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                #   He said, you look like a fine, upstanding young 
+                '  𝄿 e𝅘𝅥𝅯 e𝅘𝅥𝅯   e𝅘𝅥𝅯  e𝅘𝅥𝅯   e𝅘𝅥𝅯   e♪  d♪   c♪ c♪     d♪ |'
+
+                # man,  I think you'll do, uh, so I
+                ' d𝅘𝅥𝅯 c♪ a𝅘𝅥𝅯 d♪ d♪       c♩  𝄿 a𝅘𝅥𝅯 c𝅘𝅥𝅯 a𝅘𝅥𝅯  |'
+
+                # took off my hat and said imagine that, huh
+                ' e𝅘𝅥𝅯   e♪  c𝅘𝅥𝅯 e♪      c♪   d♪   d♪    𝄿 e𝅘𝅥𝅯  𝄾 |'
+
+                # me,   working for you,    Oh...
+                '  c♪ 𝄾  c♪      c♪ d♪  𝄾  𝄾  G♪  '  # e♪ 𝄿 e𝅘𝅥𝅯 d𝅘𝅥𝅯 e𝅘𝅥𝅯 d♪ | '
+            ),
+            sequence_part(
+                '  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  '
+                '  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  '
+                '  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  |  ',
+                sequence(
+                    random_each,
+                    measures=10,
+                    special=ActionParams(action=dimmer_sequence_flip(1)),
+                ),
+            ),
+            act_part(
+                    '  𝄻  |  ' * 10 + '  ♩ ♩  |  ',
+                    lambda: self.lights.set_relays(ALL_OFF),
+                    lambda: self.lights.set_relays(ALL_ON, special=ChannelParams()),
+            ),
+        )
+
+    def refrain(self, play_thru: int) -> Section:
+        """Signs song refrain."""
+        # 𝅝 𝅗𝅥 ♩ ♪ 𝅘𝅥𝅯 𝅘𝅥𝅰 𝄻 𝄼 𝄽 𝄾 𝄿 𝅀
+        return section(
+            bell_part(
+                # Sign, sign, everywhere a sign
+                '  e♩  e♩  d𝅘𝅥𝅯 c𝅘𝅥𝅯 c𝅘𝅥𝅯 a𝅘𝅥𝅯  c♩ |  '
+                # Blockin' out the scenery, breakin' my mind
+                '  e♪      e♪      e𝅘𝅥𝅯 d𝅘𝅥𝅯 c♪   a𝅘𝅥𝅯 c𝅘𝅥𝅯 c♪ c♩ |  '
+                # Do this, don't do that,   can't you read   the   sign?
+                '  e𝅘𝅥𝅯 e𝅘𝅥𝅯 𝄿 e𝅘𝅥𝅯   𝄿 e𝅘𝅥𝅯 c𝅘𝅥𝅯 d𝅘𝅥𝅯    𝄿 de𝅘𝅥𝅯 de𝅘𝅥𝅯 de𝅘𝅥𝅯    𝄾 d♪  | '
+                '  c♪ 𝄿 d𝅘𝅥𝅯 ' + (' 𝄽 𝄼 | ' if play_thru == 1 else ' 𝄽 𝄽 𝄾 c♪ | c♩ ')
+            ),
+            sequence_part(
+                # Sign, sign, everywhere a sign
+                '  ♪ ♪ ♩  | 𝅝  | 𝅝  | 𝅝  | ',
+                sequence(blink_all),
+                sequence(blink_all, on_first=False,
+                    special=ChannelParams(
+                        trans_off=3.5,
+                        trans_on=3.5,
+                    )
+                ),
+            ),
+        )
+ 
+    def trans (self) -> Section:
+        """Signs song trans."""
+        # 𝅝 𝅗𝅥 ♩ ♪ 𝅘𝅥𝅯 𝅘𝅥𝅰 𝄻 𝄼 𝄽 𝄾 𝄿 𝅀
+        return section(
+            drum_part(
+            '  h𝅘𝅥𝅯 l𝅘𝅥𝅯 h𝅘𝅥𝅯 l𝅘𝅥𝅯   h𝅘𝅥𝅯- l𝅘𝅥𝅯- h𝅘𝅥𝅯- l𝅘𝅥𝅯-   h𝅘𝅥𝅯> l𝅘𝅥𝅯> h𝅘𝅥𝅯> l𝅘𝅥𝅯>   h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ |'
+            # '  h𝅘𝅥𝅯 l𝅘𝅥𝅯 h𝅘𝅥𝅯 l𝅘𝅥𝅯   h𝅘𝅥𝅯- l𝅘𝅥𝅯- h𝅘𝅥𝅯- l𝅘𝅥𝅯-   h𝅘𝅥𝅯> l𝅘𝅥𝅯> h𝅘𝅥𝅯> l𝅘𝅥𝅯>   h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ |'
+            # '  h𝅘𝅥𝅯 l𝅘𝅥𝅯 h𝅘𝅥𝅯 l𝅘𝅥𝅯   h𝅘𝅥𝅯- l𝅘𝅥𝅯- h𝅘𝅥𝅯- l𝅘𝅥𝅯-   h𝅘𝅥𝅯> l𝅘𝅥𝅯> h𝅘𝅥𝅯> l𝅘𝅥𝅯>   h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ |'
+            # '  h𝅘𝅥𝅯 l𝅘𝅥𝅯 h𝅘𝅥𝅯 l𝅘𝅥𝅯   h𝅘𝅥𝅯- l𝅘𝅥𝅯- h𝅘𝅥𝅯- l𝅘𝅥𝅯-   h𝅘𝅥𝅯> l𝅘𝅥𝅯> h𝅘𝅥𝅯> l𝅘𝅥𝅯>   h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ h𝅘𝅥𝅯^ l𝅘𝅥𝅯^ |'
+            # # '  lh♩ lh♩ lh♩ lh♩ | l♪ h♪ l♪ h♪ l♪ h♪ l♪ h♪|  '
+            ),
+            sequence_part(
+                '  𝅝  ',
+                sequence(
+                    all_on,
+                    special=ChannelParams(trans_on=3.5),
+                ),
+            ),
+        )
+
