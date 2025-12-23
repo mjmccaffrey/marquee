4871856145f0d3b09a5bc7ae246c539224f176dd
KevinMcCaffrey <kjm2123@gmail.com>
2025-12-23 17:35:30 -0500

    Music Added

--- christmas.py
+++ christmas.py
@@ -26,27 +26,78 @@ class ChristmasSongs(PlayMusicMode):
         return section(
             bell_part(
                 #  Jingle Bells, Jingle Bells,
-                '  b♩ b♩   b♩ 𝄽 | b♩ b♩   b♩ 𝄽 | '
+                '  b♩ b♩   b♩ 𝄽  | b♩ b♩   b♩ 𝄽  |'
                 
                 #  Jingle all the   way 
-                '  b♩ d♩   G♩ 𝄾 a♪ | b♩ 𝄽 𝄽 𝄽 |  '
+                '  b♩ d♩   G♩ 𝄾 a♪  | b♩ 𝄽 𝄽 𝄽   |'
                 
                 # Oh what fun it    is to ride in a 
-                ' c♩ c♩    c♩ 𝄿 c♪ | c♩ b♩ b♩    b♪ b♪ |'
+                ' c♩ c♩   c♩ 𝄾 c♪ |  c♩ b♩ b♩    b♪ b♪ |'
 
                 # one horse open  sleigh
-                ' b♩  a♩    a♩ b♩ | a♩ 𝄽 d♩ 𝄽 |'
+                '  b♩ a♩   a♩ b♩   | a♩ 𝄽 d♩ 𝄽 |'
 
                 #  Jingle Bells, Jingle Bells,
-                '  b♩ b♩   b♩ 𝄽 | b♩ b♩   b♩ 𝄽 | '
+                '  b♩ b♩   b♩ 𝄽  | b♩ b♩   b♩ 𝄽  |'
                 
                 #  Jingle all the   way 
-                '  b♩ d♩   G♩ 𝄾 a♪ | b♩ 𝄽 𝄽 𝄽 |  '
+                '  b♩ d♩   G♩ 𝄾 a♪  | b♩ 𝄽 𝄽 𝄽   |'
                 
                 # Oh what fun it    is to ride in a 
-                ' c♩ c♩    c♩ 𝄿 c♪ | c♩ b♩ b♩    b♪ b♪ |'
+                ' c♩ c♩   c♩ 𝄾 c♪ |  c♩ b♩ b♩    b♪ b♪ |'
 
                 # one horse open  sleigh
-                ' d♩  d♩    b♩ G♩ | G♩ 𝄽 𝄽 𝄽 |'
+                '  d♩ d♩    c♩ a♩ | G♩ 𝄽 𝄽 𝄽  |'
             ),
+        )
+
+    def jolly_old(self) -> Section:
+        """Jolly Old St. Nicholas."""
+        # 𝅝 𝅗𝅥 ♩ ♪ 𝅘𝅥𝅯 𝅘𝅥𝅰 𝄻 𝄼 𝄽 𝄾 𝄿 𝅀
+        return section(
+            bell_part(
+                # Jolly   Old St.  Nicholas
+                '  b♪ b♪   b♪ b♪ | a♪ a♪   a♩ |'
+                
+                # Lean your ear this way
+                '  G♪ G♪   G♪ G♪   | b♩ 𝄽  |'
+
+                # Don't you tell a single soul
+                '  e♪ e♪   e♪ e♪   | D♪ D♪   G♩ |'
+                
+                # What I'm here to say
+                '  G♪ G♪  a♪ b♪   | a♩ 𝄽 |' 
+                # (Or replace the first G with F#)
+
+                # Christmas Eve is coming soon
+                '  b♪ b♪   b♪ b♪  | a♪ a♪   a♩ |'
+                
+                # Now you dear old man
+                '  G♪ G♪   G♪ G♪   | b♩ 𝄽  |'
+
+                # Whisper what you’ll bring to me
+                '  e♪ e♪   e♪ e♪     | D♪ D♪   G♩ |'
+                
+                # Tell me if you    can
+                '  a♪ G♪   a♪ b♪  | G♩ 𝄽           |'            ),
+        )
+    
+
+    def deck_the_halls(self) -> Section:
+        """Deck the Halls."""
+        # 𝅝 𝅗𝅥 ♩ ♪ 𝅘𝅥𝅯 𝅘𝅥𝅰 𝄻 𝄼 𝄽 𝄾 𝄿 𝅀
+        return section(
+            bell_part(
+                # Deck the halls with
+                ' d♩ 𝄾 c♪ b♩ a♩ |'
+                
+                # Boughs of Holly
+                ' G♩ a♩ b♩ G♩ |'
+
+                # Fa la la la la, la 
+                ' a♪ b♪ c♪ a♪ b♩ 𝄾 a♪ | ' 
+                
+                # la la la
+                ' G♩ a♩ G♩ 𝄽 |'
+                #Or replace the last a with F#
         )
