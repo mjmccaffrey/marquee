0bed7996d3f17042013c6d6ddb9d7a8c57e30bf0
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-13 16:38:18 -0500

    Modes package work etc.

--- modes/__init__.py
+++ modes/__init__.py
@@ -1,23 +1,23 @@
 """Marquee Lighted Sign Project - modes __init__"""
 
-from select_modes import BrightnessSelectMode, ModeSelectMode
+from .select_modes import BrightnessSelectMode, ModeSelectMode
 
-from christmas import ChristmasSongs
-from demo import Demo
-from evenoddfade import EvenOddFade
-from pacmanmode import PacManGame
-from randomfade import RandomFade
-from rotatereversible import RotateReversible
-from running import Running
-from sequencemode import SequenceMode
-from signs import Signs
-from silentfadebuild import SilentFadeBuild
+from .christmas import ChristmasSongs
+from .demo import Demo
+from .evenoddfade import EvenOddFade
+from .pacmanmode import PacManGame
+from .randomfade import RandomFade
+from .rotatereversible import RotateReversible
+from .running import Running
+from .sequencemode import SequenceMode
+from .signs import Signs
+from .silentfadebuild import SilentFadeBuild
 
-from chainmode import ChainMode
+from .chainmode import ChainMode
 
-from modeinterface import ModeInterface
-from backgroundmode import BackgroundMode
-from foregroundmode import ForegroundMode
+from .modeinterface import ModeInterface
+from .backgroundmode import BackgroundMode
+from .foregroundmode import ForegroundMode
 
-from mode_misc import ModeConstructor, ModeIndex
+from .mode_misc import ModeConstructor, ModeIndex
 
