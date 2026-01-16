929b4c898766849442fa2fd17e914e4fcaeec6b4
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-15 19:47:15 -0500

    DynamicTest

--- event.py
+++ event.py
@@ -62,8 +62,8 @@ class PriorityQueue:
     def push(self, event: Event) -> None:
         """Add event to queue."""
         heappush(self._queue, event)
-        print(self)
-        print()
+        # print(self)
+        # print()
         # print(f"queue length: {len(self._queue)}")
         # print(f"Event {event} added to queue.")
 
