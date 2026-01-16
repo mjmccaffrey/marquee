6891b151cadc6d8ad25c56442ee831ac6cf07957
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-15 19:44:35 -0500

    DynamicTest

--- modes/colorwheel.py
+++ modes/colorwheel.py
@@ -36,7 +36,7 @@ class ColorWheel(PerformanceMode):
                     self.lights.gamut,
                 ),
                 transition=2,
-                channel_indexes=[i + self.rotation],
+                channel_indexes=[(i + self.rotation) % 12],
             )
         self.rotation = (self.rotation + 1) % 12
         self.schedule(
