57e8374fdaee6a2796499f5324d631e8e825239b
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-25 16:07:12 -0500

    Select Modes

--- lightcontroller.py
+++ lightcontroller.py
@@ -114,7 +114,7 @@ class LightChannel(ABC):
             changes['color'] = update.color
         if update.on is not None and self.on != update.on:
             changes['on'] = update.on
-        print("CHANGES: ", changes)
+        # print("CHANGES: ", changes)
         if changes:
             return ChannelUpdate(
                 channel=update.channel,
