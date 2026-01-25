c111fee7c3a4ce4bffb917fbd748899c97cd9ac4
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-25 15:30:28 -0500

    Zones

--- specialparams.py
+++ specialparams.py
@@ -43,8 +43,8 @@ class MirrorParams(SpecialParams):
 @dataclass
 class EmulateParams(ChannelParams, MirrorParams):
     """Emulate relay experience when using smart bulbs."""
+    brightness_on = 80
     color_on = RGB(255, 197, 143)
-    color_off = RGB(255, 197, 143)
     trans_on: ClassVar[float] = 0.0
     trans_off: ClassVar[float] = 0.0
 
