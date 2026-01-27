28b5d3e5691c3f410637329cd32828067781e0fc
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-26 19:51:25 -0500

    Select Modes

--- modes/randomcolors.py
+++ modes/randomcolors.py
@@ -14,14 +14,11 @@ class RandomColors(DynamicMode):
 
     def generate_special(self) -> ChannelParams:
         """Return new set of ChannelParams."""
-        # delay = self.mode.kwargs['delay']
         return ChannelParams(
             brightness_on=100,
-            brightness_off=100,
             color_on=self.lights.colors.random(),
-            color_off=self.lights.colors.random(),
-            generate=self.generate_special,
             trans_on=0.0,
             trans_off=0.0,
+            generate=self.generate_special,
         )
 
