9419e166455a4c8ce372049a6b5c722941c99043
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-01-27 20:26:18 -0500

    Comet

--- modes/comet.py
+++ modes/comet.py
@@ -1,6 +1,6 @@
 """Marquee Lighted Sign Project - comet"""
 
-from collections.abc import Sequence
+from collections.abc import Iterable, Sequence
 from dataclasses import dataclass
 from itertools import chain
 
@@ -17,15 +17,16 @@ class Comet(PerformanceMode):
     def __post_init__(self) -> None:
         super().__post_init__()
         self.head = -1
+        self.wheel = chain(self.lights.colors.WHEEL)
         self.schedule(action=self.execute, due=self.delay, repeat=True)
-        self.color = chain(self.lights.colors.WHEEL)
 
     def execute(self) -> None:
         """"""
         count = self.lights.count
         self.head = (self.head + 1) % count
+        print(self.head)
         if self.head == 0:
-            color = next(self.color)
+            color = next(self.wheel)
         for i, c in enumerate(self.colors):
             self.lights.set_channels(
                 brightness=100 - i * 25,
