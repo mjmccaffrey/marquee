2687a5dba84e2cd6ea8fe79ea110066f31889eed
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-02-02 17:22:01 -0500

    SetupDevices

--- lightset.py
+++ lightset.py
@@ -48,7 +48,12 @@ class LightSet:
         self.gamut = self.controller.bulb_model.gamut
         RGB.adjust_incomplete_colors(self.gamut or rgbxy.GamutC)
         self.colors = Colors(self.gamut or rgbxy.GamutC)
-        assert len(self.controller.channels) == self.count
+
+
+        # Top lightset has 2 channels but 1 relay...
+        # assert len(self.controller.channels) == self.count
+
+
         self.channels = self.controller.channels
         self.trans_min = self.controller.trans_min
         self.trans_max = self.controller.trans_max
