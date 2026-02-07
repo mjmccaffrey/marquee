2ee3cac1428e04ec69285db20d53d65345330c57
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-02-07 17:19:08 -0500

    Select mode rythym

--- modes/selectmode.py
+++ modes/selectmode.py
@@ -72,7 +72,7 @@ class SelectMode(ForegroundMode, ABC):
                 kwargs=dict(
                     sequence=lambda: rotate_build_flip(count=self.desired),
                     pre_delay=0.5,
-                    delay=0.3, 
+                    delay=0.25, 
                     repeat=False,
                     init_lights=False,
                     special=self.special,
