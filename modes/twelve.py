f15807430c708dc573aacd54fbdbcbc459252e65
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-03-04 18:39:42 -0500

    Twelve

--- modes/twelve.py
+++ modes/twelve.py
@@ -37,7 +37,7 @@ class Twelve(PerformanceMode):
         # Set each color
         for i, (r, g, b) in enumerate(self.colors):
             self.lights.set_channels(
-                brightness=40,
+                brightness=25,
                 color=self.lights.colors.rgb(
                     int(r / 100 * 255),
                     int(g / 100 * 255),
