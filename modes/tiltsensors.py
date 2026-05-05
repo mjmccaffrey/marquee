d90fd9e8f311f8b4471db1cd8acc667f552c3b41
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-05-05 08:37:20 -0400

    typing-extensions

--- modes/tiltsensors.py
+++ modes/tiltsensors.py
@@ -33,6 +33,7 @@ class TiltSensors(PerformanceMode):
             shift = self.shift + direction_buttons[button]
             if -5 >= shift >= 5:
                 self.shift = shift
+                print(self.shift)
                 self.execute()
         else:
             return super().button_action(button)
