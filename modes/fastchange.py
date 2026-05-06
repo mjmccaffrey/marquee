ddbb129b5d5e5a8324893924825223c9940494d6
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-05-06 14:10:50 -0400

    fastchange

---
+++ modes/fastchange.py
@@ -0,0 +1,32 @@
+"""Marquee Lighted Sign Project - fastchange"""
+
+from dataclasses import dataclass
+import logging
+from typing_extensions import override
+
+from .performancemode import PerformanceMode
+
+log = logging.getLogger('marquee.' + __name__)
+
+
+@dataclass(kw_only=True)
+class FastChange(PerformanceMode):
+    """"""
+    delay: float
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+        self.rotation = 0
+        self.lights.set_channels(on=True, brightness=100)
+    
+    @override
+    def execute(self):
+        """"""
+        self.lights.set_channels(
+            color=self.lights.colors.random(),
+            transition=0,
+            channel_indexes={self.rotation},
+        )
+        self.rotation = (self.rotation + 1) % 12
+        self.schedule(due=self.delay)
+
