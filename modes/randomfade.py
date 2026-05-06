ebd4545512d32a8cb654cbf9f943e9adb6f4c83b
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-05-05 20:36:42 -0400

    misc

--- modes/randomfade.py
+++ modes/randomfade.py
@@ -22,6 +22,7 @@ class RandomFade(PerformanceMode):
 
     def __post_init__(self) -> None:
         """Initialize."""
+        super().__post_init__()
         self.lights.set_relays(ALL_ON)
         self.lights.set_channels(on=True)
         self.brightnesses = self.lights.brightnesses()
