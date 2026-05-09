2bf4b69714d85a53786fb759e2cf7a3082731acb
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-05-09 19:51:11 -0400

    Rotated display; pac-man

--- devices/hue.py
+++ devices/hue.py
@@ -26,7 +26,7 @@ class HueBridge(LightController, bulb_comp=HueBulb):
     """Hue bridge controller."""
 
     trans_min: ClassVar[float] = 0.0  # ?????????
-    all_at_once_supported: ClassVar[bool] = True
+    all_at_once_supported: bool = True  # !!! ClassVar
 
     application_key: str
     bulb_ids: Sequence[str]
