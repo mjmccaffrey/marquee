26124dbe0862bf67110682a42285c787ac54dfb0
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-05-12 13:30:06 -0400

    Twelve notation

--- modes/twelve.py
+++ modes/twelve.py
@@ -125,5 +125,6 @@ class Twelve(MusicMode):
             act_part(' |  ♩ 𝄽 𝄼  |  𝄻  | ', lights_off),
             part(measure(beats=3)),
             act_part(count_to_12, *lights_on),
+            act_part(' |  𝄻  | ', lights_off),
         ).play(tempo=160)
     
