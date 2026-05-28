a77b03258149e0f079d24810086a5613fe68b646
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-05-28 16:28:31 -0400

    ColorSets

--- devices/color.py
+++ devices/color.py
@@ -203,7 +203,7 @@ class ColorSets:
             name: ColorSet(
                 name, 
                 group, 
-                tuple(XYB(*c) for c in set(colors)),
+                tuple(XYB(*c) for c in set(e for e in colors)),
             )
             for name, group, colors in data
         }
