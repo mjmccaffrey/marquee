53beede43c46479f9495a36e8e9421409d535533
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-06-03 19:20:51 -0400

    PacMan joystick, CombinedLightset

--- modes/twelve.py
+++ modes/twelve.py
@@ -45,15 +45,21 @@ class Twelve(MusicMode):
             )
             for i in range(self.lights.count)
         )
-        lights_off = dict(on=False)
-        light_pairs_off = tuple(
-            dict(
-                on=False,
-                transition=0.0,
-                indices=i,
-            )
-            for i in ({0, 2}, {11, 3}, {10, 4}, {9, 5}, {8, 6}, {1, 7})
+        lights_off = dict(on=False, transition=0.0)
+        lights_rotate = tuple(
+            dict(color=Colors.WHEEL[:i] + Colors.WHEEL[i:])
+            for i in range(8)
         )
+
+
+        # light_pairs_off = tuple(
+        #     dict(
+        #         on=False,
+        #         transition=0.0,
+        #         indices=i,
+        #     )
+        #     for i in ({0, 2}, {11, 3}, {10, 4}, {9, 5}, {8, 6}, {1, 7})
+        # )
         count_to_12 = ' |  ♪ ♪ ♪ ♩ ♩ ♪  |  ♩ ♪ ♩ ♩ ♪  |  𝄽 ♩  | '
         piece(
             section(
@@ -63,7 +69,8 @@ class Twelve(MusicMode):
             lights(count_to_12, *lights_on),
             lights(' |  𝄽 𝄽  | ', beats=2),
             # lights(' |  𝄼 ♩ ♩  |  ♩ ♩ ♩ ♩  | ', *light_pairs_off),
-            lights(' |  𝄻  |  𝄻  | '),
+            # lights(' |  𝄻  |  𝄻  | '),
+            lights(' |  ♩ ♩ ♩ ♩  |  ♩ ♩ ♩ ♩  | ', *lights_rotate),
             lights(' |  𝄽 ♩ 𝄽  | ', lights_off, beats=3),
             lights(count_to_12, *lights_on),
             lights(' |  𝄻  |  ♩  | ', lights_off),
