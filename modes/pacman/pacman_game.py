d73138a884a6b7193f74e7df80d22d38b82599f2
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-06-04 13:34:57 -0400

    misc

--- modes/pacman/pacman_game.py
+++ modes/pacman/pacman_game.py
@@ -244,6 +244,7 @@ class PacManGame(GameMode):
             return
         if self.tick == 16:
             self.place_entity(self.fruit, self.fruit_start)
+            self.update_lights(self.board)  # ???? !!!!
 
     def ghost_got_pacman(self) -> bool:
         """"""
