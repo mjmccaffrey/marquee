ece0ae20c5950891b2cf0976caf1749734da52be
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-06-24 17:41:52 -0400

    CS Dynamic

--- modes/colorsetdynamic.py
+++ modes/colorsetdynamic.py
@@ -55,8 +55,8 @@ class ColorSetDynamic(ColorSetMode):
         kwargs: dict[str, Any] = dict(
             baseline=LightSetBaseline(on=False),
             color_set_name=entry.name,
-            delay=2, 
-            special=ChannelParams(),
+            delay=1, 
+            special=ChannelParams(trans_off=0),
         )
         sequence_kwargs: dict[str, Any] = dict(
             clockwise=self.clockwise,
