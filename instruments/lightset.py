dacb2aead810b1bc19297609bfb3f1f2beac4717
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-06-25 19:07:12 -0400

    light groups

--- instruments/lightset.py
+++ instruments/lightset.py
@@ -133,6 +133,7 @@ class LightSet:
         # no parameter sequences => group update possible
 
         _index = self._convert_index(index)
+        print(f"{_index=}")
         updates = channel_updates()
         group = self.controller.indices_in_group.get(frozenset(_index))
         if group is not None and no_params_are_sequences():
