5d6a3f19f8586fd9837d61251ae8879f4885c942
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-07-14 15:38:58 -0400

    mode instances, effect new mode, generated modes

---
+++ modes/generatedmodes.py
@@ -0,0 +1,46 @@
+"""Marquee Lighted Sign Project - generatedmodes"""
+
+from dataclasses import dataclass
+from typing_extensions import override
+
+from . import RandomFade, ModeDefinition
+
+
+@dataclass(kw_only=True)
+class GeneratedModes(RandomFade):
+    """"""
+    light_index: int | None  # None => parent
+
+    @override
+    def execute(self) -> None:
+        """"""
+        if self.light_index is None:
+            for light_index in range(self.lights.count):
+                self.generate_mode(light_index)
+            self.schedule(self.exit, 120)
+        else:
+            self.update_light_schedule_next(self.light_index)
+
+    def exit(self) -> None:
+        """"""
+        self.delete_mode_instance(self.index)
+
+    def generate_mode(self, light_index: int):
+        """"""
+        mode = self.create_mode_instance(
+            mode_definition=ModeDefinition(
+                name='cs_rotate',
+                cls=RandomFade,
+            ),
+            kwargs=dict(
+                background=True,
+                parent=self,
+                transition=self.transition,
+                duration=self.duration,
+                color_set_name=self.color_set_name,
+                light_index=light_index,
+            ),
+            parent=self,
+        )
+        self.schedule(mode.execute)
+
