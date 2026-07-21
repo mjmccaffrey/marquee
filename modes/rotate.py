fc287523e18d26272c8dd61c4ed40986c9f86ab1
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-07-21 15:16:38 -0400

    rotate sides

--- modes/rotate.py
+++ modes/rotate.py
@@ -39,10 +39,11 @@ class RotateSides(PerformanceMode):
             color=EmulateParams.color_on,
             transition=EmulateParams.trans_on,
             index=current,
+            force=True,
         )
         self.lights.set_channels(
             on=False,
-            # transition=EmulateParams.trans_off,
+            transition=EmulateParams.trans_off,
             index=self.previous,
         )
         self.previous = current
