2ccd4e793d3c432bbddfce1a5c025546d6fc2a02
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-07-23 15:03:12 -0400

    2026_sign

--- modes/twelve.py
+++ modes/twelve.py
@@ -36,11 +36,11 @@ class Twelve(MusicMode):
             dict(
                 on=True,
                 brightness=100,
+                color=c,
                 transition=0.0,
                 index=(i + 2) % self.lights.count,
-                force=True,
             )
-            for i in range(self.lights.count)
+            for i, c in enumerate(Colors.WHEEL)
         )
         each_off = tuple(
             dict(
