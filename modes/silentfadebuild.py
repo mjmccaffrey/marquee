4ce0f535120b21a7c78cd18cc8b43e94fb4133e8
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-07-24 09:59:09 -0400

    2026_sign

--- modes/silentfadebuild.py
+++ modes/silentfadebuild.py
@@ -14,6 +14,7 @@ class SilentFadeBuild(PerformanceMode):
 
     def __post_init__(self) -> None:
         """Initialize."""
+        super().__post_init__()
         self.lights.set_channels(brightness=0)
  
     @override
