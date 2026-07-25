3225a055997d2c6411112b4658c9e3bd77649e17
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-07-25 10:13:17 -0400

    2026_sign

--- modes/pause.py
+++ modes/pause.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 from itertools import cycle, repeat
 import logging
+from typing_extensions import override
 
 from devices.color import Color
 from light_defs import EXTRA_CUPOLA
@@ -13,4 +14,9 @@ log = logging.getLogger('marquee.' + __name__)
 
 @dataclass(kw_only=True)
 class Pause(PerformanceMode):
-    """Do nothing."""
+    """Do almost nothing."""
+
+    @override
+    def __post_init__(self):
+        super().__post_init__()
+        self.lights.set_channels(on=False, force=True)
