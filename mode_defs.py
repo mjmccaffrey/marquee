357ce6f3c284e840c04bf691ad33033e3541e4d2
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-09-19 14:04:44 -0400

    WIP

--- mode_defs.py
+++ mode_defs.py
@@ -131,7 +131,7 @@ def register_channel_modes(exec: Executor):
         pattern='0123456789abcdef',
         mask='-***************',
         clockwise=True,
-        delay=2.0,
+        delay=10.0,
         brightness=35,
     )
     exec.add_mode("cs_chase_3", ColorSetDynamic, 
