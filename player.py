7fd96b41bbffd4d9c7d0bd2a5c11fbff6750f55d
Mike McCaffrey <mjmccaffrey@gmail.com>
2026-09-19 12:59:09 -0400

    WIP

--- player.py
+++ player.py
@@ -159,7 +159,7 @@ class Player:
 
     def wait(self, seconds: float | None) -> None | NoReturn:
         """"""
-        print("PLAYER.WAIT")
+        log.info("PLAYER.WAIT")
         if self.interrupt_trigger.wait(seconds):
             assert self.interrupt is not None
             print("RAISING", self.interrupt)
